ada7dd05028d8ca2c08368b6d11c106387fb4c0a
Ilya_Donnik <59254180+koshkadog@users.noreply.github.com>
2023-06-13 14:02:52 +1000

    Add files via upload

---
+++ chemistry_helper.py
@@ -0,0 +1,15 @@
+import PySimpleGUI as sg
+import windows
+import functions
+
+if __name__ == '__main__':
+    window = windows.window_main()
+    while True:
+        event, close = window.read()
+        if close == sg.WIN_CLOSED:
+            break
+        if event == '-RXN_B-':
+            functions.rxn_b()
+        if event == '-Substance_B-':
+            functions.subs()
+    window.close()
---
+++ functions.py
@@ -0,0 +1,49 @@
+import PySimpleGUI as sg
+import chempy as cm
+import windows
+
+
+def rxn_b():
+    window_rxn = windows.window_rxn()
+    while True:
+        event_rxn, rxn = window_rxn.read()
+        if event_rxn == sg.WIN_CLOSED:
+            break
+        if event_rxn == '-balance_stoichiometry-':
+            try:
+                rxn['-RXN-'] = rxn['-RXN-'].split('=')
+                left = ''.join(rxn['-RXN-'][0].split('+'))
+                right = ''.join(rxn['-RXN-'][1].split('+'))
+                reac, prod = left.split(), right.split()
+                reac, prod = cm.balance_stoichiometry(reac, prod)
+                reac, prod = dict(reac), dict(prod)
+                for sub in reac:
+                    if reac[sub] == 1:
+                        reac[sub] = 1
+                for sub in prod:
+                    if prod[sub] == 1:
+                        prod[sub] = 1
+                rxn_balanced = cm.Reaction(reac, prod).string()
+                window_rxn['-OUTPUT-'].update(f'Сбалансированная реакция: {rxn_balanced}')
+            except Exception:
+                sg.popup('Введите корректные данные', title='Error')
+                pass
+
+
+def subs():
+    window_substance = windows.window_substance()
+    while True:
+        event_substance, substance = window_substance.read()
+        if event_substance == sg.WIN_CLOSED:
+            break
+        if event_substance == '-SUB_INFO-':
+            try:
+                sub = cm.Substance.from_formula(substance['-Substance-'])
+                window_substance['-OUTPUT-'].update(f'Информация о веществе: {sub.unicode_name}\n\n'
+                                                    f'Формула в формате latex: {sub.latex_name}\n\n'
+                                                    f'Формула в формате html: {sub.html_name}\n\n'
+                                                    f'Заряд: {sub.charge}\n\n'
+                                                    f'Молярная масса {sub.unicode_name} = {sub.molar_mass()}')
+            except Exception:
+                sg.popup('Введите корректные данные', title='Error')
+                pass
